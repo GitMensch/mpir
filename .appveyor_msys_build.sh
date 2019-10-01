51bbc2cf35d75650bab59fe18a597da64f205879
Simon Sobisch <simonsobisch@web.de>
2019-10-01 22:06:25 +0200

    appveyor with more build options and binaries #280

--- .appveyor_msys_build.sh
+++ .appveyor_msys_build.sh
@@ -1,13 +1,17 @@
 export PATH=/c/msys64/mingw$ABI/bin:/c/projects/mpir/bin/:$PATH
 cd /c/projects/mpir
+
 echo && echo build: ./autogen.sh
 ./autogen.sh
+
 case "$LIBRARY" in
 	"static") LIB="--enable-static --disable-shared";;
 	"shared") LIB="--disable-static --enable-shared";;
 esac
+
 echo && echo build: ./configure ABI=$ABI $LIB $CONFIG_CXX $CONFIG_GMP
 ./configure ABI=$ABI $LIB $CONFIG_CXX $CONFIG_GMP
+
 echo && echo build: make
 make
 # should work but falsely requires texlive ?!?
@@ -15,13 +19,16 @@ make
 #DISTCHECK_CONFIGURE_FLAGS="ABI=$ABI $LIB" make distcheck
 echo && echo build: make check
 make check
+
 echo && echo build: make dist
 make dist
-if ! test -z "$CONFIG_GMP"; then
-	if ! test -z "$CONFIG_CXX"; then
+
+if test "x$CONFIG_GMP" != "x"; then
+	if test "x$CONFIG_CXX" != "x"; then
 		MODE="C++"
 	else
 		MODE=""
 	fi
+	echo && echo build: make install DESTDIR=$(pwd)/bin$ABI$MODE-$LIBRARY
 	make install DESTDIR=$(pwd)/bin$ABI$MODE-$LIBRARY
 fi
